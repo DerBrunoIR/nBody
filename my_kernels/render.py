f22daef2584fcffb47d5f506d7c467d9a94e912d
Bruno Rothschild <bruno.iganz.rothschild@gmail.com>
2024-09-05 05:53:03 +0200

    add bezier interpolation

--- my_kernels/render.py
+++ my_kernels/render.py
@@ -6,6 +6,7 @@ import math
 MAX_DIM = 3
 DEFAULT_DTYPE = types.float32
 MINIMAL_DISTANCE = 1
+MAXIMAL_DISTANCE = 10000
 
 
 @cuda.jit
@@ -43,37 +44,68 @@ def convert_planets_to_image(dt, global_positions, global_velocities, global_mas
         mass = global_masses[n]
 
         # DREHUNG
-        angle = dt / 1000
+        #angle = dt / 1000
         # turn around x axis
         # pos[1] = math.cos(angle) * y - math.sin(angle) * z
         # pos[2] = math.sin(angle) * y + math.cos(angle) * z
         # turn around y axis
-        pos[0] = math.cos(angle) * pos[0] + math.sin(angle) * pos[2]
-        pos[2] = -math.sin(angle) * pos[0] + math.cos(angle) * pos[2]
+        #pos[0] = math.cos(angle) * pos[0] + math.sin(angle) * pos[2]
+        #pos[2] = -math.sin(angle) * pos[0] + math.cos(angle) * pos[2]
         # turn around z axis
         # pos[0] = math.cos(angle) * x - math.sin(angle) * y
         # pos[1] = math.sin(angle) * x + math.cos(angle) * y
 
 
-        # calculate distance
+        # calculate pixel color
         squared_rad = MINIMAL_DISTANCE
+        background = (0x38, 0x3e, 0x70)
+        color1 = (0xf0, 0x9e, 0x00)
+        color2 = (0xf0, 0xd8, 0x00)
+        color3 = (255, 0, 0x0b)
+        wght = 15
+        distr = (0, .5 * wght, 1 * wght, math.inf)
+
         for i in range(2):
             squared_rad += (pos[i] - local_pixel_position[i])**2
-        squared_rad += 0.002 * pos[2]**2
-        r = mass / (15 * squared_rad)
+        #squared_rad += 0.002 * pos[2]**2
+        w = mass / math.sqrt(squared_rad)
 
-        local_pixel_color[0] += 24 * r
-        local_pixel_color[1] += 8 * r
-        local_pixel_color[2] += 4* r
-        local_pixel_color[3] += 0
+        local_pixel_color[0] += rclamp(background[0], color1[0], distr[0], distr[1], w) + rclamp(color1[0], color2[0], distr[1], distr[2], w) + rsclamp(color2[0], color3[0], distr[2], distr[3], w)
+        local_pixel_color[1] += rclamp(background[1], color1[1], distr[0], distr[1], w) + rclamp(color1[1], color2[1], distr[1], distr[2], w) + rsclamp(color2[1], color3[1], distr[2], distr[3], w)
+        local_pixel_color[2] += rclamp(background[2], color1[2], distr[0], distr[1], w) + rclamp(color1[2], color2[2], distr[1], distr[2], w) + rsclamp(color2[2], color3[2], distr[2], distr[3], w)
 
+    local_pixel_color[3] = 255 * count_of_bodies
 
     for i in range(global_image.shape[2]):
-        # local_pixel_color[i] = min(1, max(local_pixel_color[i], 0))
-        local_pixel_color[i] = 1 - min(1, max(local_pixel_color[i], 0))
+        local_pixel_color[i] = min(1, max(local_pixel_color[i] / (255 * count_of_bodies), 0))
     
     for i in range(global_image.shape[2]):
         global_image[y_idx][x_idx][i] = local_pixel_color[i]
+
+
+@cuda.jit
+def clamp(a, b, t):
+    # assert 0 <= t <= 1
+    return a + (b - a) * min(max(t, 0), 1)
+
+
+@cuda.jit
+def rclamp(a, b, r1, r2, r):
+    if r < r1 or r > r2:
+        return 0
+    rr = (r - r1) / (r2 - r1)
+    return clamp(a, b, rr)
+
+@cuda.jit
+def rsclamp(a, b, r1, r2, r):
+    if r < r1:
+        return 0
+    if r > r2:
+        return b
+    return clamp(a, b, (r - r1) / (r2 - r1))
+
+
+
 
 
 @cuda.jit
